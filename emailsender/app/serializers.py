5044c1175dce6a7415461bb676e6083c488e0f1b
Nimo08 <nimomohamed800@gmail.com>
2024-06-20 17:00:27 +0300

    create serializers

---
+++ emailsender/app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Subscriber, EmailContent
+
+class SubscriberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscriber
+        fields = ['email', 'subscribed']
+
+class EmailContent(serializers.ModelSerializer):
+    class Meta:
+        model = EmailContent
+        fields = ['title', 'body', 'created_at']
